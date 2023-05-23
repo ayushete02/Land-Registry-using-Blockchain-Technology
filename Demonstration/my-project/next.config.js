af4289e544a2ec1013ec006c3b069c0c7c630302
Ayush Shete <75811912+ayushete02@users.noreply.github.com>
2023-05-24 02:07:55 +0530

    Update next.config.js

--- Demonstration/my-project/next.config.js
+++ Demonstration/my-project/next.config.js
@@ -1,16 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   reactStrictMode: true,
 }
 
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  nextConfig,
-  images: {
-    "loader": "akamai",
-    "path": ""
-  },
-  trailingSlash: true,
-  assetPrefix: isProd ? "https://your-domain.com" : "",
-}
+module.exports = nextConfig
